2df541e51449965ee9dfa2cb456ade14105b4eb8
Mottakin890 <mottakinulalam@gmail.com>
2025-12-25 23:20:03 +0600

    feat(color): App color scheme added successfully

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+//Colors
+const Color bgColor = Color(0xFFD2FFF4);
+const Color primaryColor = Color(0xFF2D5D70);
+const Color secondaryColor = Color(0xFF265DAB);
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
